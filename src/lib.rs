bb317b33b3644dc87f500c448316052631979d8e
Hytak <lhc.holten@gmail.com>
2021-06-08 22:59:14 +0200

    Update the ffi

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,107 @@
-#[link(wasm_import_module = "imports")]
-extern "C" {
-    pub fn energize(index: usize, target_index: usize);
+#![allow(clashing_extern_declarations)]
 
-    #[link_name = "move"]
-    pub fn goto(index: usize, x: f32, y: f32);
+pub mod spirit {
+    #[link(wasm_import_module = "spirits")]
+    extern "C" {
+        pub fn energize(index: usize, target_index: usize);
 
-    pub fn merge(index: usize, target_index: usize);
+        #[link_name = "energizeBase"]
+        pub fn energize_base(index: usize);
 
-    pub fn divide(index: usize);
+        #[link_name = "energizeEnemyBase"]
+        pub fn energize_enemy_base(index: usize);
 
-    pub fn jump(index: usize, x: f32, y: f32);
+        #[link_name = "move"]
+        pub fn goto(index: usize, x: f32, y: f32);
 
-    #[link_name = "spiritCount"]
-    pub fn spirit_count() -> usize;
+        pub fn merge(index: usize, target_index: usize);
 
-    #[link_name = "spiritIsFriendly"]
-    pub fn spirit_is_friendly(index: usize) -> bool;
+        pub fn divide(index: usize);
 
-    #[link_name = "spiritPositionX"]
-    pub fn spirit_position_x(index: usize) -> f32;
+        pub fn jump(index: usize, x: f32, y: f32);
 
-    #[link_name = "spiritPositionY"]
-    pub fn spirit_position_y(index: usize) -> f32;
+        pub fn count() -> usize;
 
-    #[link_name = "spiritSize"]
-    pub fn spirit_size(index: usize) -> f32;
+        #[link_name = "isFriendly"]
+        pub fn is_friendly(index: usize) -> bool;
 
-    #[link_name = "spiritEnergyCapacity"]
-    pub fn spirit_energy_capacity(index: usize) -> f32;
+        #[link_name = "positionX"]
+        pub fn position_x(index: usize) -> f32;
 
-    #[link_name = "spiritEnergy"]
-    pub fn spirit_energy(index: usize) -> f32;
+        #[link_name = "positionY"]
+        pub fn position_y(index: usize) -> f32;
 
-    #[link_name = "spiritHp"]
-    pub fn spirit_hp(index: usize) -> f32;
+        pub fn size(index: usize) -> f32;
 
-    #[link_name = "basePositionX"]
-    pub fn base_position_x() -> f32;
+        #[link_name = "energyCapacity"]
+        pub fn energy_capacity(index: usize) -> f32;
 
-    #[link_name = "basePositionY"]
-    pub fn base_position_y() -> f32;
+        pub fn energy(index: usize) -> f32;
 
-    #[link_name = "baseSize"]
-    pub fn base_size() -> f32;
+        pub fn hp(index: usize) -> f32;
+    }
+}
 
-    #[link_name = "baseEnergyCapacity"]
-    pub fn base_energy_capacity() -> f32;
+pub mod base {
+    #[link(wasm_import_module = "base")]
+    extern "C" {
+        #[link_name = "positionX"]
+        pub fn position_x() -> f32;
 
-    #[link_name = "baseEnergy"]
-    pub fn base_energy() -> f32;
+        #[link_name = "positionY"]
+        pub fn position_y() -> f32;
 
-    #[link_name = "baseHp"]
-    pub fn base_hp() -> f32;
+        pub fn size() -> f32;
 
-    #[link_name = "enemyBasePositionX"]
-    pub fn enemy_base_position_x() -> f32;
+        #[link_name = "energyCapacity"]
+        pub fn energy_capacity() -> f32;
 
-    #[link_name = "enemyBasePositionY"]
-    pub fn enemy_base_position_y() -> f32;
+        pub fn energy() -> f32;
 
-    #[link_name = "enemyBaseSize"]
-    pub fn enemy_base_size() -> f32;
+        pub fn hp() -> f32;
+    }
+}
 
-    #[link_name = "enemyBaseEnergyCapacity"]
-    pub fn enemy_base_energy_capacity() -> f32;
+pub mod enemy_base {
+    #[link(wasm_import_module = "enemyBase")]
+    extern "C" {
+        #[link_name = "positionX"]
+        pub fn position_x() -> f32;
 
-    #[link_name = "enemyBaseEnergy"]
-    pub fn enemy_base_energy() -> f32;
+        #[link_name = "positionY"]
+        pub fn position_y() -> f32;
 
-    #[link_name = "enemyBaseHp"]
-    pub fn enemy_base_hp() -> f32;
+        pub fn size() -> f32;
 
-    #[link_name = "starCount"]
-    pub fn star_count() -> usize;
+        #[link_name = "energyCapacity"]
+        pub fn energy_capacity() -> f32;
 
-    #[link_name = "spiritPositionX"]
-    pub fn star_position_x(index: usize) -> f32;
+        pub fn energy() -> f32;
 
-    #[link_name = "spiritPositionY"]
-    pub fn star_position_y(index: usize) -> f32;
+        pub fn hp() -> f32;
+    }
+}
+
+pub mod star {
+    #[link(wasm_import_module = "base")]
+    extern "C" {
+        pub fn count() -> usize;
+
+        #[link_name = "positionX"]
+        pub fn position_x(index: usize) -> f32;
+
+        #[link_name = "positionY"]
+        pub fn position_y(index: usize) -> f32;
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn tick() {
     unsafe {
-        let (x, y) = (base_position_x(), base_position_y());
-        for index in 0..spirit_count() {
-            if spirit_is_friendly(index) {
-                goto(index, x, y)
+        let (x, y) = (base::position_x(), base::position_y());
+        for index in 0..spirit::count() {
+            if spirit::is_friendly(index) {
+                spirit::goto(index, x, y)
             }
         }
     }
