3064ce9a02c70c48005ed60e7416bf1000d21fb3
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-06-16 20:31:35 +0200

    Add constant and helper function for star

--- src/star.rs
+++ src/star.rs
@@ -1,5 +1,15 @@
 use crate::position::Position;
 
+/// Small stars only begin producing energy
+/// after this amount of ticks have passed.
+pub const SMALL_START_TICK: u32 = 100;
+
+/// Each tick a star generates 3 + 1% of its current energy,
+/// rounded to the nearest integer.
+pub fn next_energy(energy: u32) -> u32 {
+    ((energy + 3) as f32 * 1.01).round() as u32
+}
+
 #[link(wasm_import_module = "stars")]
 extern "C" {
     /// Get the number of stars in the game.
