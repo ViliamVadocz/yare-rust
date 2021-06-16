d56d847c24813d308a73114ec878a7373a491dbc
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-06-16 20:30:11 +0200

    Add documentation for modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(clashing_extern_declarations)]
-// #![warn(missing_docs)]
+#![warn(missing_docs)]
 
 //! You should export a tick function with the following signature:
 //! ```
@@ -7,11 +7,22 @@
 //! pub extern "C" fn tick(tick: u32) {}
 //! ```
 
+/// The base creates new spirits once it has enough energy.
+/// If your base if destroyed you lose the game.
 pub mod base;
+/// Mappings for the JavaScript console.
 pub mod console;
+/// Yare.io is a RTS game played by executing code.
 pub mod game;
+/// Outposts are objects that can be captured by energizing them.
+/// Outposts will shoot at random energies within their range.
 pub mod outpost;
+/// Information about players in the game.
 pub mod player;
+/// Position is given as an x and y coordinate on an Euclidean plane.
 pub mod position;
+/// Spirits are the game's units. They carry energy which is used for creating
+/// new spirits, fighting, and capturing outposts.
 pub mod spirit;
+/// Stars are sources of energy.
 pub mod star;
