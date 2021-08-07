294f7b1e1bb28d78a56c46d835627183cbac954f
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-08-07 19:59:34 +0200

    Update maximum game length

--- src/bindings/game.rs
+++ src/bindings/game.rs
@@ -3,4 +3,4 @@ pub const WIDTH: f32 = 4400.;
 /// Height of the game board.
 pub const HEIGHT: f32 = 2700.;
 /// Maximum game length in ticks.
-pub const MAX_GAME_LEN: u32 = 2400;
+pub const MAX_GAME_LEN: u32 = 3000;
