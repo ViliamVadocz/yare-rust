47a7743bee9c8b08fd2fb3b27bdb8b561724bb2d
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-08-28 17:25:31 +0200

    Formatting

--- src/headless/yare_impl/mod.rs
+++ src/headless/yare_impl/mod.rs
@@ -9,7 +9,7 @@ mod star;
 mod vec2;
 
 pub(crate) use command::Command;
-pub use headless::{Headless, Outcome, SimulationResult, BotFn};
+pub use headless::{BotFn, Headless, Outcome, SimulationResult};
 pub(crate) use headless::{BASES, COMMANDS, ME, OUTPOSTS, PLAYER_NUM, SPIRITS, STARS};
 pub use shape::Shape;
 pub(crate) use vec2::Vec2;
